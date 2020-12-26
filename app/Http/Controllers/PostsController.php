a059eabd7841c492247b10ad35722a1b0c462d1e
Colissa Pollard <colissapollard@appointmentplus.com>
2020-12-26 00:49:00 -0700

    Playing around with Eloquent Models.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostsController extends Controller {
     public function show($slug) {
-        $post = DB::table('posts')->where('slug', $slug)->first();
 
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
